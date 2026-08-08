3328d058a246651b41ff17e1ad3c0a52b0591c69
pashtopoetry <pashtodubbingsongsayede7@gmail.com>
2026-08-08 23:11:26 +0330

    build: update gradle execution command
    Switch from using the jar manifest to explicitly setting the classpath and main class to improve wrapper execution reliability.

--- android-project/gradlew.bat
+++ android-project/gradlew.bat
@@ -3,4 +3,4 @@ if not exist gradle\wrapper\gradle-wrapper.jar (
   echo Downloading Gradle wrapper jar...
   powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/gradle/gradle/raw/v8.5.0/gradle/wrapper/gradle-wrapper.jar', 'gradle\wrapper\gradle-wrapper.jar')"
 )
-java -jar gradle\wrapper\gradle-wrapper.jar %*
+java -classpath gradle\wrapper\gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain %*
